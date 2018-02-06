32c7af4cfb9fe3edd2be34adbfa0e5f3e24ef740
tzejian <akernus.lr@gmail.com>
2018-02-06 13:53:41 +0800

    controller folder for all the logic files

---
+++ controller/dbFunctions.php
@@ -0,0 +1,13 @@
+<?php
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "rogerproject";
+$link = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+
+if (!$link) {
+    die(mysqli_error($link));
+    // alternative way to display the error:
+    // die(mysqli_connect_error());
+}
+?>
